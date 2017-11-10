f1e75ef1fb154911af91b02c885a832dbe5d985c
americast <sayan.sinha@iitkgp.ac.in>
2017-11-11 02:28:14 +0530

    add_smtp

--- kwoc/sendgrid_mail.py
+++ kwoc/sendgrid_mail.py
@@ -13,23 +13,96 @@ import os
 from sendgrid.helpers.mail import *
 import traceback
 import requests
-
+import os
+import sis
 import utils
+import smtplib
 
 
 def send_mail(mail_subject, mail_body, to_email):
-    sg = sendgrid.SendGridAPIClient(apikey=os.environ["DEFCON_SENDGRID"])
-    from_email = Email("Kharagpur Open Source Society <{}>".format(os.environ["EMAIL"]))
-    content = Content("text/html", mail_body)
-    to_email = Email(to_email)
-    mail = Mail(from_email=from_email, subject=mail_subject, to_email=to_email, content=content)
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    #Next, log in to the server
+    #print(str(os.environ['PASSWD']))
+    server.login(str(os.environ['EMAIL']), str(os.environ['PASSWD']))
+
+    from email.mime.multipart import MIMEMultipart
+    from email.mime.text import MIMEText
+
+    me = "Kharagpur Winter of Code <kwoc@kossiitkgp.in>"
+    you = to_email
+
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = mail_subject
+    msg['From'] = me
+    msg['To'] = you
+    #Send the mail
+    html = mail_body
+    # html ="""\
+    # <html>
+    #   <head></head>
+    #   <body>
+    #     <p>Hello mentor!<br>
+    #        Welcome to KWoC!<br>
+    #        Please read the manual <a href="https://kwoc.kossiitkgp.in/static/files/KWoCMentorManual.pdf">here</a>.
+    #     </p>
+    #   </body>
+    # </html>
+    # """ # The /n separates the message from the headers
+
+    #part1 = MIMEText(text, 'plain')
+    part2 = MIMEText(html, 'html')
+
+    #msg.attach(part1)
+    msg.attach(part2)
+
     try:
-        sg.client.mail.send.post(request_body=mail.get())
-        return True
-    except urllib.HTTPError:
-        msg = "Got following error while sending mail :{}".format(traceback.format_exc())
-        utils.slack_notification(msg)
-        return False
+        server.sendmail(me, you, msg.as_string())
+        print("Sent")
+        print(i+1)
+    except:
+        try:
+            del(server)
+            os.system("sleep 5m")
+            server = smtplib.SMTP('smtp.gmail.com', 587)
+            server.starttls()
+            #Next, log in to the server
+            #print(str(os.environ['PASSWD']))
+            server.login(str(os.environ['EMAIL']), str(os.environ['PASSWD']))
+            server.sendmail(me, you, msg.as_string())
+            print("Sent")
+        except:
+            try:
+                del(server)
+                os.system("sleep 5m")
+                server = smtplib.SMTP('smtp.gmail.com', 587)
+                server.starttls()
+                #Next, log in to the server
+                #print(str(os.environ['PASSWD']))
+                server.login(str(os.environ['EMAIL']), str(os.environ['PASSWD']))
+                server.sendmail(me, you, msg.as_string())
+                print("Sent")
+            except:
+                msg = "Got error while sending mail to "+to_email
+                utils.slack_notification(msg)
+                return False
+
+
+
+'''Not used, for sendgrid
+sg = sendgrid.SendGridAPIClient(apikey=os.environ["DEFCON_SENDGRID"])
+from_email = Email("Kharagpur Open Source Society <{}>".format(os.environ["EMAIL"]))
+content = Content("text/html", mail_body)
+to_email = Email(to_email)
+mail = Mail(from_email=from_email, subject=mail_subject, to_email=to_email, content=content)
+try:
+    sg.client.mail.send.post(request_body=mail.get())
+    return True
+except urllib.HTTPError:
+    msg = "Got following error while sending mail :{}".format(traceback.format_exc())
+    utils.slack_notification(msg)
+    return False
+'''
 
 
 
